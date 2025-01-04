cc4f3b4255ee61728dae017b85c99a77b01b74dd
RemFdz <remi.fernandez@epitech.eu>
2025-01-04 19:30:57 +0100

    fix(build): remove old name for abstract packet and add packet factory files empty

---
+++ src/RType/Network/Packets/PacketFactory/PacketFactory.cpp
@@ -0,0 +1,8 @@
+/*
+** EPITECH PROJECT, 2025
+** RType
+** File description:
+** TODO: add description
+*/
+
+#include "PacketFactory.hpp"
---
+++ src/RType/Network/Packets/PacketFactory/PacketFactory.hpp
@@ -0,0 +1,8 @@
+/*
+** EPITECH PROJECT, 2025
+** RType
+** File description:
+** Factory used to create a packet from asio buffer
+*/
+
+#pragma once
