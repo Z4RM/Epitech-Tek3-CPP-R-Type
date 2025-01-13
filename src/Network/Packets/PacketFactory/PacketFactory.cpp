d4961aa76ce7b55c2f1fb8bd728186cb4e567743
Clement-Z4RM <cloviswakfu@gmail.com>
2025-01-13 16:57:42 +0100

    refactor(packet-factory): revert "fix clang-tidy and compilation warnings"
    This reverts commit c6eb492309f7b34429e03a407e7d875f8909496b.

--- src/Network/Packets/PacketFactory/PacketFactory.cpp
+++ src/Network/Packets/PacketFactory/PacketFactory.cpp
@@ -14,19 +14,22 @@
 
 namespace rtype::network {
     std::unique_ptr<IPacket> PacketFactory::fromCode(int code) {
-        if (code == EPacketCode::CONNECT)
-            return std::make_unique<PacketConnect>();
-        throw PacketFactoryException();
+        switch (code) {
+            case EPacketCode::CONNECT:
+                return std::make_unique<PacketConnect>();
+            default: throw PacketFactoryException();
+        }
     }
 
     std::unique_ptr<IPacket> PacketFactory::fromBuffer(const std::vector<char> &buffer) {
         int code = 0;
         std::memcpy(&code, buffer.data(), sizeof(int));
-        if (code == CONNECT) {
-            std::unique_ptr<PacketConnect> packet = std::make_unique<PacketConnect>();
-            packet->fillData(buffer);
-            return packet;
+        switch (code) {
+            case CONNECT:
+                std::unique_ptr<PacketConnect> packet = std::make_unique<PacketConnect>();
+                packet->fillData(buffer);
+                return packet;
         }
-        throw PacketFactoryException();
     }
+
 }
