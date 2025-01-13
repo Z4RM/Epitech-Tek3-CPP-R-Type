5f33cb772a7a5932d8c3a32581cc1c784b670d85
MathisZUCCHERO <mathis.zucchero@epitech.eu>
2025-01-13 17:14:13 +0100

    feet(sprite): add rect in sprite

--- src/RType/Components/Client/Sprite.hpp
+++ src/RType/Components/Client/Sprite.hpp
@@ -23,17 +23,20 @@ namespace rtype::components {
             created.isCreate = false;
             texture = new sf::Texture();
             sprite = new sf::Sprite();
+            rect = new sf::RectangleShape();
             size = Size();
             size.width = value["size"]["width"];
             size.height = value["size"]["height"];
             texture->loadFromFile(path);
             sprite->setTexture(*texture);
             sprite->setScale(size.width / texture->getSize().x, size.height / texture->getSize().y);
+            rect->setSize({size.width, size.height});
         }
         std::string path;
         ZIndex priority;
         sf::Texture* texture;
         sf::Sprite* sprite;
+        sf::RectangleShape *rect;
         Created created;
         Size size;
     };
