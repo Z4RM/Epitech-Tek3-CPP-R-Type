8b06d9e5f852bdd896bd7fdcb80c85c40c7e2db8
Clement-Z4RM <cloviswakfu@gmail.com>
2025-02-11 20:25:20 +0100

    docs(systems): add doxygen documentation on sound system methods

--- src/RType/Systems/Sound/Sound.hpp
+++ src/RType/Systems/Sound/Sound.hpp
@@ -29,8 +29,24 @@ namespace rtype::systems {
          */
         static void play(ecs::EntityManager &entityManager, ecs::ComponentManager &componentManager);
 
+        /**
+         * @brief Creates a new sound effect using the ECS framework.
+         *
+         * @param path The path to the sound effect file.
+         * @param componentManager A reference to the `ComponentManager` for managing sound-related components.
+         * @param entity The entity ID to associate with the new sound effect.
+         */
         static void createEffect(const std::string &path, ecs::ComponentManager &componentManager, unsigned int entity);
 
+        /**
+         * @brief Creates a new music using the ECS framework.
+         *
+         * @param path The path to the music file.
+         * @param componentManager A reference to the `ComponentManager` for managing music-related components.
+         * @param entity The entity ID to associate with the new music.
+         * @param loop Whether the music should loop or not.
+         * @param duration The duration of the music, if you want to play only a part of it (optional).
+         */
         static void createMusic(
                 const std::string &path,
                 ecs::ComponentManager &componentManager,
